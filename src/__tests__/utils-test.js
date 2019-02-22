5a6d46da9d03ca30418692a682b5baa06fdac05f
Alex Kudlick <adkudlick@gmail.com>
2019-02-21 16:12:50 -0800

    utils - add extra requirements
    wheewwwwie this turned out to be harder than it should have been. I
    want to easily be able to say something like
    `this.between = utils.requireArrayOfLength('between', between, 2)`.
    
    I needed to write a lot of infrastructure for writing and testing
    matchers.

--- src/__tests__/utils-test.js
+++ src/__tests__/utils-test.js
@@ -2,19 +2,92 @@ import utils from '../utils';
 
 describe('utils', () => {
   describe('requirePresent', () => {
-    it('throws if the argument is not present', () => {
-      expect(() => utils.requirePresent('coolArg', null)).toThrow('coolArg is required');
-      expect(() => utils.requirePresent('coolArg', undefined)).toThrow('coolArg is required');
-    });
-
-    it('returns the argument if present', () => {
-      expect(utils.requirePresent('coolArg', 'asdf')).toEqual('asdf');
+    it('requires the argument to be present', () => {
+      expectThrows(utils.requirePresent, ['coolArg', undefined], 'Expected coolArg to be present; got undefined.');
+      expectThrows(utils.requirePresent, ['coolArg', null], 'Expected coolArg to be present; got null.');
+      expectReturnsArgument(utils.requirePresent, ['coolArg', 'asdf']);
     });
 
     it('handles only one argument', () => {
-      expect(utils.requirePresent('asdf')).toEqual('asdf');
-      expect(() => utils.requirePresent(null)).toThrow('is required');
-      expect(() => utils.requirePresent(undefined)).toThrow('is required');
+      expectHandlesOnlyOneArgument(utils.requirePresent, [
+        { input: undefined, isValid: false},
+        { input: null, isValid: false},
+        { input: 'asdf', isValid: true},
+      ]);
     });
   });
+
+  describe('requireArray', () => {
+    it('requires the argument to be an array', () => {
+      expectThrows(utils.requireArray, ['name', undefined], 'Expected name to be an array; got undefined.');
+      expectThrows(utils.requireArray, ['name', 'asdf'], 'Expected name to be an array; got "asdf".');
+      expectReturnsArgument(utils.requireArray, ['name', []]);
+      expectReturnsArgument(utils.requireArray, ['name', ['1']]);
+      expectReturnsArgument(utils.requireArray, ['name', ['1', '2']]);
+
+      expectHandlesOnlyOneArgument(utils.requireArray, [
+        { input: undefined, isValid: false},
+        { input: 'asdf', isValid: false},
+        { input: ['asdf'], isValid: true},
+      ]);
+    });
+  });
+
+  describe('requireArrayOfLength', () => {
+    it('requires the argument to be an array of the specified length', () => {
+      const requireWithLengthOne = (...input) => {
+        input = input.concat([1]);
+        return utils.requireArrayOfLength(...input)
+      };
+
+      expectThrows(requireWithLengthOne, ['name', undefined], 'Expected name to be an array; got undefined.');
+      expectThrows(requireWithLengthOne, ['name', 'asdf'], 'Expected name to be an array; got "asdf".');
+      expectThrows(requireWithLengthOne, ['name', []], 'Expected name to be an array of length 1; got [].');
+      expectThrows(requireWithLengthOne, ['name', ['1', '2']], 'Expected name to be an array of length 1; got ["1","2"].');
+      expectReturnsArgument(requireWithLengthOne, ['name', ['1']]);
+      expectReturnsArgument(requireWithLengthOne, [['1']]);
+    });
+  });
+
+  const expectHandlesOnlyOneArgument = (func, testCases) => {
+    testCases.forEach(testCase => {
+      testCase = Object.assign(
+        {},
+        testCase,
+        { input: [testCase.input] }
+      );
+      return expectHandlesTestCase(func, testCase)
+    });
+  };
+
+  const expectHandlesTestCase = (func, testCase) => {
+    if (testCase.isValid) {
+      expectReturnsArgument(func, testCase.input);
+    } else {
+      expectThrows(func, testCase.input, testCase.message);
+    }
+  };
+
+  const expectThrowsOnOneInput = (func, input, expectedMessage) => {
+    expectThrows(func, [input], expectedMessage);
+  }
+
+  const expectThrows = (func, input, expectedMessage) => {
+    if (!Array.isArray(input)) {
+      input = [input];
+    }
+    expectedMessage = expectedMessage || 'Expected';
+    const assertionError = `Expected an error containing '${expectedMessage}' on input ${input}`;
+    expect(() => func(...input), assertionError).toThrow(expectedMessage);
+  };
+
+  const expectReturnsArgument = (func, input) => {
+    if (!Array.isArray(input)) {
+      input = [input];
+    }
+
+    const expected = input[input.length - 1];
+    const assertionErrorMessage = `Expected to return input value ${JSON.stringify(expected)} on input ${JSON.stringify(input)}`;
+    expect(func(...input), assertionErrorMessage).toEqual(expected);
+  };
 });
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,75 @@
-export default {
-  requirePresent: function() {
-    let name;
-    let obj;
-    if (arguments.length === 2) {
-      name = arguments[0];
-      obj = arguments[1];
-    } else {
-      name = 'argument';
-      obj = arguments[0];
-    }
+const extractArgs = (args, ...extraArgNames) => {
+  let name;
+  let value;
+  let extraArgStartingIndex;
+  if (args.length >= 2 + extraArgNames.length) {
+    name = args[0];
+    value = args[1];
+    extraArgStartingIndex = 2;
+  } else {
+    name = 'argument';
+    value = args[0];
+    extraArgStartingIndex = 1;
+  }
 
-    if (!obj) {
-      throw new Error(name + ' is required');
-    } else {
-      return obj;
-    }
-  },
+  const extraArgValues = {};
+  extraArgNames.forEach((argName, i) => {
+    extraArgValues[argName] = args[extraArgStartingIndex + i];
+  });
+
+  return Object.assign({ name, value }, extraArgValues);
+}
+
+const requireCondition = (pass, value, message) => {
+  if (!pass) {
+    throw new Error(message);
+  } else {
+    return value;
+  }
+}
+
+const expectationMessage = (name, value, requirement) => {
+  try {
+    value = JSON.stringify(value);
+  } catch {
+    // Ignore
+  }
+
+  return `Expected ${name} to be ${requirement}; got ${value}.`;
+
+}
+
+export const requirePresent = function() {
+  const { name, value } = extractArgs(arguments);
+  return requireCondition(
+    !!value,
+    value,
+    expectationMessage(name, value, 'present')
+  );
+}
+
+export const requireArray = function() {
+  const { name, value } = extractArgs(arguments);
+  return requireCondition(
+    Array.isArray(value),
+    value,
+    expectationMessage(name, value, 'an array')
+  );
+}
+
+export const requireArrayOfLength = function() {
+  const { name, value, n } = extractArgs(arguments, 'n');
+  const array = requireArray(name, value);
+
+  return requireCondition(
+    requireArray(value).length === n,
+    value,
+    expectationMessage(name, value, 'an array of length ' + n)
+  );
+}
+
+export default {
+  requirePresent,
+  requireArray,
+  requireArrayOfLength,
 }
