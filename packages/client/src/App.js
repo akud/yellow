d811802333b1fd1466af85eb8dacf990d9f3f220
Alex Kudlick <adkudlick@gmail.com>
2019-03-28 15:43:57 -0700

    Revert "Add plugin to transform class properties"
    This reverts commit aea8939feee4d225c14f6a08cff579bd0e9f11d8.

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Circle from './elements/components/Circle';
+import Image from './elements/components/Image';
 import Rectangle from './elements/components/Rectangle';
 import Label from './elements/components/Label';
 import Orientation from './elements/Orientation';
