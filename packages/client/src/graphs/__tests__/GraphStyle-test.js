d9cb5db12c14990f85d81e13e78ca923c104f974
Alex Kudlick <adkudlick@gmail.com>
2020-01-02 10:42:16 -0800

    GrpahStyle-test - fix test with new strength values

--- packages/client/src/graphs/__tests__/GraphStyle-test.js
+++ packages/client/src/graphs/__tests__/GraphStyle-test.js
@@ -9,9 +9,12 @@ describe('GraphStyle', () => {
   describe('Default', () => {
     it('renders a centering rule and repelling rule', () => {
       const wrapper = shallow(<GraphStyle.Default />);
+
       expect(wrapper.find(Rules.CenteringRule).length).toBe(1);
+      expect(wrapper.find(Rules.CenteringRule).prop('strength')).toBe(0.5);
+
       expect(wrapper.find(Rules.RepellingRule).length).toBe(1);
-      expect(wrapper.find(Rules.RepellingRule).prop('strength')).toBe(10);
+      expect(wrapper.find(Rules.RepellingRule).prop('strength')).toBe(25);
     });
   });
 });
